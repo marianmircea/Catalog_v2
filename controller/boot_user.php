d94fc9c68971af77f7ff789b4f9cb42a06e815c7
marianmircea <marian.mircea01@yahoo.com>
2018-05-01 19:56:44 +0300

    upd 1mai v2

--- controller/boot_user.php
+++ controller/boot_user.php
@@ -1,18 +1,18 @@
 <?php
 	//Controller de afisare pe tip de utilizator ...
-	echo "suntem in boot user";
-	
-	require '/../model/session.php';
+	echo $_GET["tip"]."<br>";
+	echo $_GET["id"]."<br>";
+	//require '/../model/session.php';
 	class Bootuser {
 		function __construct () {
-			//require 'logged.php';
-			//$aff = new logged();
-			//$aff -> index();
+			echo "suntem in boot user<br>";
+			require 'logged.php';
+			$aff = new logged();
+			$aff -> index();
 			
-			echo $_GET["tip"]."<br>";
-			echo $_GET["id"]."<br>";
+			//header ('refresh:1, url=../view/under_con/');
 			
-			Session::init();
+			//Session::init();
 		}
 	}
 	$tt = new Bootuser;
--- view/index/index.php
+++ view/index/index.php
@@ -1,4 +1,2 @@
 <h2>CN Ion Creanga Bistrita - Catalog online</h2>
 <h4>Bine ati venit!</h4>
-<?php
-	echo $_SERVER['PHP_SELF'];
