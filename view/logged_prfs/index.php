9a40ee1394d67cd6bbbb723c7ef414a12e7fc47c
marianmircea <marian.mircea01@yahoo.com>
2018-05-02 20:33:45 +0300

    upd 2mai v1

--- view/logged_prfs/index.php
+++ view/logged_prfs/index.php
@@ -1,3 +1,5 @@
 pagina pentru profesor<br>
 <?php
-	require 'D:/wamp/www/Catalog_v2/view/logged/index.php';
+	require 'D:/wamp/www/Catalog_v2/controller/logged.php';
+	$aff = new logged();
+	$aff -> index();
